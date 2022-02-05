a80b536455564b5888fd99f57afcf86e03590741
ss0g <55930267+ss0g@users.noreply.github.com>
2022-02-04 19:13:07 -0800

    fixed stuff perhaps

--- src/main/java/com/spartronics4915/frc2022/commands/LauncherCommands.java
+++
@@ -1,9 +0,0 @@
-package com.spartronics4915.frc2022.commands;
-
-import com.spartronics4915.frc2022.subsystems.Launcher;
-
-public class LauncherCommands {
-    private final Launcher mLauncher;
-
-    
-}
