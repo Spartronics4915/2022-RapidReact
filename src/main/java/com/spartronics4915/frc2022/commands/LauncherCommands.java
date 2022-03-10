a7919ad9826f8e4af6e5a8c79ce84cb9fba4afbc
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2022-03-09 17:42:52 -0800

    remove default commands, switch to scheduling commands in RobotContainer constructor, thx henry

--- src/main/java/com/spartronics4915/frc2022/commands/LauncherCommands.java
+++ src/main/java/com/spartronics4915/frc2022/commands/LauncherCommands.java
@@ -17,7 +17,6 @@ public class LauncherCommands
     public LauncherCommands(Launcher subsystem, Joystick mArcadecontroller)
     {
         mLauncher = subsystem;
-        mLauncher.setDefaultCommand(new ToggleLauncher());
     }
 
     public class ToggleLauncher extends CommandBase
